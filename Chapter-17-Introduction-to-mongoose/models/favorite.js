307e5aae92567af0b313560369cd93f5495fac89
Ravi Kumar <ravibseb96@gmail.com>
2025-04-17 21:40:10 +0530

    chapter 17 mongoose started

---
+++ Chapter-17-Introduction-to-mongoose/models/favorite.js
@@ -0,0 +1,28 @@
+module.exports = class Favourite {
+  constructor(homeId) {
+    this.homeId = homeId;
+  }
+
+  save() {
+    const db = getDb();
+    return db
+      .collection("favorites")
+      .findOne({ homeId: this.homeId })
+      .then((existingFav) => {
+        if (!existingFav) {
+          return db.collection("favorites").insertOne(this);
+        }
+        return Promise.resolve();
+      });
+  }
+
+  static getFavourites(callback) {
+    const db = getDb();
+    return db.collection("favorites").find().toArray();
+  }
+
+  static deleteById(delHomeId) {
+    const db = getDb();
+    return db.collection("favorites").deleteOne({ homeId: delHomeId });
+  }
+};
